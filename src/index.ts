dcaa9f85631db1075a973fa695e586f9a211bf9b
Lastav1ca <stefandmd22@gmail.com>
2026-08-17 19:40:14 +0200

    added server initialization and error logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-function main(){
-    console.log("Hello, World!")
+import express, { type Express, type Request, type Response } from 'express';
+import { middlewareLogResponses } from './middleware.js';
+
+const app: Express = express();
+const port = 8080;
+
+app.use("/app", express.static("./src/app"));
+
+app.use(middlewareLogResponses);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/healthz", handlerReadiness);
+
+function handlerReadiness(req : Request, res : Response){
+    res.set('Content-Type', 'text/plain');
+    res.status(200)
+    res.send('OK')
 }
 
-main();
---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response  } from "express";
+
+
+export const middlewareLogResponses = (req : Request, res : Response, next : NextFunction) : void => {
+    res.on("finish", () => {
+        const statusCode = res.statusCode;
+        if (statusCode != 200){
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`)
+        }
+    });
+    
+    next()
+};
+
