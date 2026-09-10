b4b7a30d35d8db60fc805e39aad5ac9891696d5e
Lastav1ca <stefandmd22@gmail.com>
2026-09-10 20:14:26 +0200

    organized index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { type Express, type Request, type Response } from 'express';
 import { middlewareErrors, middlewareLogResponses, middlewareMetricsInc } from './middleware.js';
 import { config } from './config.js';
-import { handlerGetChirp, handlerGetAllChirps, handlerCreateChirp, handlerCreateUser, handlerReadiness, handlerRequestsNum, handlerRequestsNumReset } from './handlers.js';
+import { handlerLogin, handlerGetChirp, handlerGetAllChirps, handlerCreateChirp, handlerCreateUser, handlerReadiness, handlerRequestsNum, handlerRequestsNumReset } from './handlers.js';
 
 import postgres from "postgres";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
@@ -14,25 +14,26 @@ const app: Express = express();
 const port = 8080;
 
 app.use(express.json())
+app.use(middlewareLogResponses);
+
+//admin
+app.post("/admin/reset", handlerRequestsNumReset);
+app.get("/admin/metrics", handlerRequestsNum);
+
+//users & auth
+app.post("/api/users", handlerCreateUser);
+app.post("/api/login", handlerLogin)
+
+//chirps
+app.get("/api/chirps", handlerGetAllChirps)
+app.get("/api/chirps/:chirpId", handlerGetChirp)
+app.post("/api/chirps", handlerCreateChirp);
+
+//misc
+app.get("/api/healthz", handlerReadiness);
 
 app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
-
-app.use(middlewareLogResponses);
-
-app.get("/admin/metrics", handlerRequestsNum);
-
-app.post("/api/users", handlerCreateUser);
-
-app.post("/admin/reset", handlerRequestsNumReset);
-
-app.post("/api/chirps", handlerCreateChirp);
-
-app.get("/api/healthz", handlerReadiness);
-
-app.get("/api/chirps", handlerGetAllChirps)
-
-app.get("/api/chirps/:chirpId", handlerGetChirp)
 
 app.use(middlewareErrors);
 
